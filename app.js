09d6e436babd8ae833ce42b55f66391763e9e461
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2024-09-23 14:16:56 +0100

    create the game control function
    add event listener to each cell

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function createPlayer(name, marker) {
 // factory function for the board module
 const gameBoard = (() => {
   let board = Array.from(cells);
-
+  
   const getBoard = function() {
     return board;
   }
@@ -31,6 +31,7 @@ const gameBoard = (() => {
   }
 
   const checkForWin = function() {
+    console.log('called');
     const winConditions = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
       [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
@@ -38,14 +39,15 @@ const gameBoard = (() => {
 
     for(let condition of winConditions) {
       const [a, b, c] = condition;
-      
-      if(board[a] && board[a] == board[b] && board[a] == board[c]) {
+      console.log(a)
+      if(board[a].textContent && board[a].textContent == board[b].textContent && board[a].textContent == board[c].textContent) {
         return true;
       }else {
         return false;
       }
     }
   }
+  checkForWin()
 
   const isBoardFull = function() {
     return board.every(box => box.textContent != '');
@@ -59,4 +61,53 @@ const gameBoard = (() => {
     isBoardFull
   }
 
+})(); // the board function(IIFE) ends here is immediately called
+
+const gameControl = (function() {
+  let player1, player2, currentPlayer;
+  
+  const startGame = function(name1, marker1, name2, marker2) {
+    player1 = createPlayer(name1, marker1);
+    player2 = createPlayer(name2, marker2);
+    currentPlayer = player1;
+
+    // reset game board
+    gameBoard.resetBoard();
+  };
+
+  const playTurn = function(index) {
+    if(gameBoard.getBoard()[index].textContent == '') {
+      gameBoard.setMove(index, currentPlayer.getMarker());
+
+      if(gameBoard.checkForWin()) {
+        console.log(`${currentPlayer.name} Wins`);
+      }else if(gameBoard.isBoardFull()) {
+        return `it's a draw`;
+      }else {
+        switchTurn();
+      }
+    }
+    return null;
+  }
+
+  const switchTurn = function() {
+    currentPlayer = (currentPlayer == player1) ? player2 : player1;
+  }
+
+  const getCurrentPlayer = function() {
+    return currentPlayer;
+  }
+
+  return {
+    startGame,
+    playTurn,
+    getCurrentPlayer
+  }
 })();
+
+gameControl.startGame('james', 'o', 'shady', 'x');
+Array.from(cells).forEach((box, index) => {
+  box.addEventListener('click', () => {    
+    gameControl.playTurn(index);
+  })
+})
